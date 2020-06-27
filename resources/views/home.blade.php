4eb92b8a03a214f8a9f71c4cfaf6c2b6e10cc0f8
thanos thanasis <you@example.com>
2020-06-27 04:14:20 +0300

    test

--- resources/views/home.blade.php
+++
@@ -1,9 +0,0 @@
-<x-master>
-
-     <x-home.header></x-home.header>
-
-     <x-home.tabs :popularMovies="$popularMovies" :genreMovies="$genreMovies" :latestMovie="$latestMovie" :topRateMovie="$topRateMovie" ></x-home.tabs>
-
-    <x-home.carousel :movies="$upComingMovie" :genreMovies="$genreMovies"></x-home.carousel>
-
-</x-master>
